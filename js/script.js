bd6346289d9d78e9da05d375214fda24ee49fc06
MelikovRoman <77622004+MelikovRoman@users.noreply.github.com>
2021-09-07 19:39:07 +0300

    Добавляет секцию Команда и слайдер товара

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+///// Навигация
+
 const hamburger  = document.querySelector('.hamburger');
 const overlay = document.querySelector('.overlay');
 const body = document.querySelector('body');
@@ -21,6 +23,8 @@ function toggleMenu(){
 
 hamburger.addEventListener('click' , toggleMenu);
 
+///// Выпадающее меню (товар)
+
 const product_button  = document.querySelector('.product__button');
 const dropdown_content  = document.querySelector('.dropdown__content');
 
@@ -29,3 +33,90 @@ function dropdown(){
 }
 
 product_button.addEventListener('click' , dropdown);
+
+///// Слайдшоу (отзывы)
+
+const findBlockByAlias = alias => {
+  return $(".reviews__item").filter((ndx, item) => {
+    return $(item).attr("data-linked-with") == alias
+  })
+}
+
+$(".interactive-avatar__link").click(e => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-open");
+  const itemToShow = findBlockByAlias(target);
+  const curItem = $this.closest(".interactive-avatar");
+
+  itemToShow.addClass("reviews__item--active").siblings().removeClass("reviews__item--active");
+  curItem.addClass("interactive-avatar--active").siblings().removeClass("interactive-avatar--active");
+})
+
+///// Вертикальный аккардеон (команда)
+
+const openItem = item => {
+  const container = item.closest(".team__item");
+  const contentBlock = container.find(".team__content");
+  const textBlock = contentBlock.find(".team__content-block")
+  const reqHeight = textBlock.height()
+
+  container.addClass("active")
+  contentBlock.height(reqHeight);
+}
+
+const closeEveryItem = container => {
+  const items = container.find(".team__content");
+  const itemContainer = container.find(".team__item");
+
+  itemContainer.removeClass("active");
+  items.height(0);
+}
+
+$(".team__button").click (e => {
+  const $this = $(e.currentTarget);
+  const container = $this.closest(".team");
+  const elemContainer = $this.closest(".team__item")
+
+  if (elemContainer.hasClass("active")) {
+    closeEveryItem(container)
+  } else {
+    closeEveryItem(container)
+    openItem($this)
+  }
+})
+
+///// Слайдер (товар)
+
+const leftBtn = document.querySelector(".product-slider__errow-left");
+const rightBtn = document.querySelector(".product-slider__errow-right");
+const items = document.querySelector(".products");
+const computedStyle = getComputedStyle(items);
+const item = document.querySelectorAll(".products__item");
+
+const minRight = 0;
+const itemWidth = getComputedStyle(item[0]).width;
+const step = parseInt(itemWidth);
+const maxRight = (item.length - 1) * step;
+let currentRight = 0;
+
+items.style.right = currentRight;
+
+rightBtn.addEventListener("click", e => {
+  e.preventDefault();
+
+  if(currentRight < maxRight) {
+    currentRight += step;
+    items.style.right = currentRight + "px"
+  }
+})
+
+leftBtn.addEventListener("click", e => {
+  e.preventDefault();
+
+  if(currentRight > minRight) {
+    currentRight -= step;
+    items.style.right = currentRight + "px"
+  }
+});
